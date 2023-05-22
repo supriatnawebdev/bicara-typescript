4434a9c418ec557e2a26b1eafc7badc7f30dfc66
supriatnawebdev <yusupsupriatna442@gmailc.om>
2023-05-22 21:37:23 +0700

    belajar classes dan interface,type

--- src/6-class.ts
+++ src/6-class.ts
@@ -9,3 +9,65 @@
 
 // class --> OOP ( Object Oriented Programming)
 // class --> Object Instance
+// class --> Object Instance vs Object Literal
+
+abstract class Root {
+    abstract done: boolean;
+}
+
+class User extends Root {
+    // properties
+    id: number;
+    firstName: string;
+    lastName: string;
+    protected save: boolean;
+    private token: string;
+    static MAX_FAILED_LOGIN = 2;
+    private retryLogin = 0;
+    done: boolean;
+
+    // method
+    login(username: string, password: string) {
+        this.retryLogin += 1;
+        if (username == 'admin' && password == 'admin') {
+            return true;
+        }
+
+        if (this.retryLogin >= User.MAX_FAILED_LOGIN) {
+            return 'batas login sudah habis';
+        }
+        return false;
+    }
+    register() {}
+
+    constructor(id: number, firstName: string, lastName: string) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.save = false;
+        this.token = '';
+        this.done = false;
+    }
+}
+// memanggil static tanpa inisialisasi
+User.MAX_FAILED_LOGIN = 3;
+let myUser = new User(1, 'Yusup', 'Supriatna');
+console.log(myUser.login('', ''));
+console.log(myUser.login('', ''));
+console.log(myUser.login('', ''));
+
+// tidak dapat memanggil save di sini
+
+class EnhacementUser extends User {
+    location: string;
+    constructor(location: string, id: number, firstName: string, lastName: string) {
+        // memanggil super constructor User
+        super(id, firstName, lastName);
+        this.location = location;
+        // dapat memanggil save di ini
+        this.save;
+    }
+}
+
+let myNewUser = new EnhacementUser('IDN', 1, 'Yusup', 'Supriatna');
--- src/7-interface_vs_type.ts
+++ src/7-interface_vs_type.ts
@@ -6,17 +6,101 @@ export {}; // quick fix for global variable
  * Object
  */
 
+// use -> name, age
+interface User {
+    name: string;
+    age: number;
+}
+type TypeUser = {
+    name: string;
+    age: number;
+};
+
 /**
  * Merge
  */
+
+interface Song {
+    songName: string;
+}
+interface Song {
+    artisName: string;
+}
+
+const mySong: Song = {
+    artisName: 'Nella Kharisma',
+    songName: 'Jaran Goyang',
+};
 
 /**
  * Intersection & Union
  */
 
+type typeA = {
+    id: number;
+    propA: string;
+};
+type typeB = {
+    id: number;
+    propB: string;
+    propC?: string;
+};
+
+// interection 1
+type IntersectionAB = typeA & typeB;
+
+// union
+type UnionAB = typeA | typeB;
+
+let myDataIntersection: IntersectionAB = {
+    id: 1,
+    propA: 'testA',
+    propB: 'testB',
+    // propC: 'testC' opsional karena memakai tanda ?
+};
+
+let myDataUnion: UnionAB = {
+    id: 1,
+    propB: 'testB',
+    // propC: 'testC' opsional karena memakai tanda ?
+};
+
 /**
  * Implements
  */
+
+// type Person = {
+//     name: string;
+//     age: number;
+//     getName(id: number, name: string): string;
+// }
+
+interface Addres {
+    street: string;
+}
+
+interface Person extends Addres {
+    name: string;
+    age: number;
+    street: string;
+    getName(id: number, name: string): string;
+}
+
+class People implements Person {
+    name: string;
+    age: number;
+    street: string;
+
+    constructor(name: string, age: number, street: string) {
+        this.name = name;
+        this.age = age;
+        this.street = '';
+    }
+
+    getName(id: number, name: string): string {
+        return 'OK';
+    }
+}
 
 /**
  * Extend
